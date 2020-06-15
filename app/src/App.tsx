6196b9015f2802d16feee634a6a213920b61b7e8
askAguilr <dafsoft@gmail.com>
2020-06-14 22:08:53 -0500

    3d ui

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -4,9 +4,67 @@ import {
   SocketProvider,
   useSocketState
 } from 'react-socket-io-hooks';
+import foreheadMarker from '../src/assets/forehead-marker.png';
+import mouthMarker from '../src/assets/mouth-marker.png';
+import facemaskOk from '../src/assets/facemask-ok.png';
+import facemaskFail from '../src/assets/facemask-fail.png';
+import thermoGreen from '../src/assets/temp-green.png';
+import thermoNone from '../src/assets/temp-none.png';
+import thermoAmber from '../src/assets/temp-amber.png';
+import thermoRed from '../src/assets/temp-red.png';
 import SubWindow  from './SubWindow';
 
 const exec = eval('require')('child_process').exec;
+
+const Thermometer = (props:any) => {
+  const {color} = props;
+  const renders = {
+    NONE:()=><img src={thermoNone} alt='' style={{
+      width:280,
+      height:'auto',
+      position:'absolute',
+      left:1324,
+      top:0,
+    }}/>,
+    GREEN:()=><img src={thermoGreen} alt='' style={{
+      width:280,
+      height:'auto',
+      position:'absolute',
+      left:1324,
+      top:0,
+    }}/>,
+    AMBER:()=><img src={thermoAmber} alt='' style={{
+      width:280,
+      height:'auto',
+      position:'absolute',
+      left:1324,
+      top:0,
+    }}/>,
+    RED:()=><img src={thermoRed} alt='' style={{
+      width:280,
+      height:'auto',
+      position:'absolute',
+      left:1324,
+      top:0,
+    }}/>,
+
+  }
+  //ts-ignore
+  const ret = renders[color]?renders[color]():renders['NONE']();
+  return(ret);
+}
+
+const Mask = (props:any) => {
+  const {mask} = props;
+  return <img src={mask?facemaskOk:facemaskFail} alt='' style={{
+    width:550,
+    height:'auto',
+    position:'absolute',
+    left:-600,
+    top:0,
+  }}/>
+}
+
 
 function App() {
   useEffect( () => {
@@ -32,14 +90,22 @@ function App() {
     uri="http://localhost:555"
     initialState={{color:'NONE'}}> 
       <div className="App">
-        <div style={{
-          position: 'absolute',
-          right: 100,
-          top: 200
-        }}>
-            <span className="thermometer"></span>
-        </div>
-          
+        <img src={foreheadMarker} alt='' style={{
+          width:800,
+          height:'auto',
+          position:'absolute',
+          left:500,
+          top:-300,
+        }}/>
+        <Thermometer color={color}/>
+        <Mask mask={true}/>
+        <img src={mouthMarker} alt='' style={{
+          width:800,
+          height:'auto',
+          position:'absolute',
+          left:-200,
+          top:600,
+        }}/>          
       </div>
       </SocketProvider>
   );
